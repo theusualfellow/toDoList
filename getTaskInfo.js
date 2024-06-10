194b8ae5a24f08323f8ba2a362b403218c5daed5
sandeep <sandeepsingh23ok@gmail.com>
2024-06-10 02:32:40 -0300

    Make check priority appear when button clicked

---
+++ getTaskInfo.js
@@ -0,0 +1,15 @@
+export class getTaskInformation{
+    static getTitle(){
+        const title = document.querySelector("#title").value
+        return title
+    }
+    static getDescription(){
+        const description = document.querySelector("#description").value
+        return description
+    }
+    static getPriority(){
+        const priority = document.querySelector("input[name=priority]:checked").value
+        return priority
+    }
+    
+}
