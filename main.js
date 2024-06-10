ec31bdd58127187925d73cc33ecabf58f7e59b63
sandeep <sandeepsingh23ok@gmail.com>
2024-06-10 01:54:54 -0300

    Break main into modules

--- main.js
+++ main.js
@@ -1,48 +1,23 @@
+import { Task } from "./task.js"
+import { getTaskInformation } from "./getTaskInfo.js"
+//
 const button = document.querySelector("#okay")
 const task = document.querySelector(".task")
 const allTasks = []
 
 button.addEventListener("click",()=>{
-    const addTask =  new Task(getTitle(), getDescription(), getPriority())
-    addTask.addTitle()
-    addTask.addDescription()
-    addTask.addPriority()
-    allTasks.push(addTask)
-    console.log(allTasks)
+    const newTask =  new Task(
+        getTaskInformation.getTitle(), 
+        getTaskInformation.getDescription(), 
+        getTaskInformation.getPriority()
+    )
+    task.appendChild(newTask.addTitle())
+    task.appendChild(newTask.addDescription())
+    task.appendChild(newTask.addPriority())
+    allTasks.push(newTask)
+    console.log(newTask)
 })
 
-function getTitle(){
-    const title = document.querySelector("#title").value
-    return title
-}
-function getDescription(){
-    const description = document.querySelector("#description").value
-    return description
-}
-function getPriority(){
-    const priority = document.querySelector("#priority").value
-    return priority
-}
 
-class Task{
-    constructor(title, description, priority){
-        this.title = title
-        this.description = description
-        this.priority = priority
-    }
-    addTitle(){
-        const h1 = document.createElement('h1')
-        h1.innerText = getTitle()
-        task.appendChild(h1)
-    }
-    addDescription(){
-        const paragraph = document.createElement('p')
-        paragraph.innerText = getDescription()
-        task.appendChild(paragraph)
-    }
-    addPriority(){
-        const priority = document.createElement('h2')
-        priority.innerText= getPriority()
-        task.appendChild(priority)
-    }
-}
+
+
